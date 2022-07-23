1e5d6c33341c2e613d871702207dc20c873bdc84
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-23 18:53:39 +0900

    $this-> でメソッド呼び出し

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -69,6 +69,8 @@ class Login extends Controller
 
         //アクセス
       //  dd($decoded_data['access_token']);
+
+      //エラーが解決しないので急しのぎだが
        $access_token="eyJhbGciOiJIUzI1NiJ9.8UfdPW_1j1pNobRJMei71J_7SwIv2GYqOUzKxYm5v9pgCIrIaCS81LHoPoy62iaRsAJHNrDFK-OfTSnR-YVn_z_KJNey1VGtVgec_2ZOTjrNyq-D-oHhk2TTpNmLTru-I65nzwXXnM-Anuekh6wV7Haa0NpmYUMwLfS_yt7YwN0.fYUbi8GRoLpSNG9nIFst3iKJHb_CLBbu9CW_VNphR74";
      /*
       //DBに格納
@@ -80,7 +82,7 @@ class Login extends Controller
 
         $logUser ->save();
 */
-        getProf($access_token);
+        $this->getProf($access_token);
   
 
 
