4fa75bfe5e3ff3dc26090a745ebb3a19b80a3677
sayuko-furuichi <zack.dog009@gmail.com>
2022-07-24 13:08:50 +0900

    うーん？

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -14,8 +14,8 @@ class Login extends Controller
     private $code;
     private $encUrl;
 
-    // private LoginUser $logU; 
-    private $logU; 
+    // private LoginUser $logU;
+    private $logU;
 
     public function login()
     {
@@ -43,7 +43,8 @@ class Login extends Controller
     }
 
 
-    public function callback(Request $request){
+    public function callback(Request $request)
+    {
         $this->code= $request->code;
 
         $api_url ='https://api.line.me/oauth2/v2.1/token';
@@ -55,9 +56,9 @@ class Login extends Controller
 
         curl_setopt($curl_handle, CURLOPT_POST, true);
         curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=$this->code&redirect_uri=https://dev-ext-app.herokuapp.com/public/callback&client_id=1657292332&client_secret=1b8433d37832199bf746a66e7d8a5a77" );
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, "grant_type=authorization_code&code=$this->code&redirect_uri=https://dev-ext-app.herokuapp.com/public/callback&client_id=1657292332&client_secret=1b8433d37832199bf746a66e7d8a5a77");
         curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
-                // curl_exec()の結果を文字列にする
+        // curl_exec()の結果を文字列にする
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
         
 
@@ -71,49 +72,42 @@ class Login extends Controller
         $decoded_data = json_decode($json_response, true);
 
         //アクセス
-      //  dd($decoded_data['access_token']);
+        //  dd($decoded_data['access_token']);
 
-      //エラーが解決しないので急しのぎだが
-       $access_token="eyJhbGciOiJIUzI1NiJ9.8UfdPW_1j1pNobRJMei71J_7SwIv2GYqOUzKxYm5v9pgCIrIaCS81LHoPoy62iaRsAJHNrDFK-OfTSnR-YVn_z_KJNey1VGtVgec_2ZOTjrNyq-D-oHhk2TTpNmLTru-I65nzwXXnM-Anuekh6wV7Haa0NpmYUMwLfS_yt7YwN0.fYUbi8GRoLpSNG9nIFst3iKJHb_CLBbu9CW_VNphR74";
+        //エラーが解決しないので急しのぎだが
+        $access_token="eyJhbGciOiJIUzI1NiJ9.8UfdPW_1j1pNobRJMei71J_7SwIv2GYqOUzKxYm5v9pgCIrIaCS81LHoPoy62iaRsAJHNrDFK-OfTSnR-YVn_z_KJNey1VGtVgec_2ZOTjrNyq-D-oHhk2TTpNmLTru-I65nzwXXnM-Anuekh6wV7Haa0NpmYUMwLfS_yt7YwN0.fYUbi8GRoLpSNG9nIFst3iKJHb_CLBbu9CW_VNphR74";
    
-       /*
+        /*
       //DBに格納
-        $logUser =new LoginUser;
-        $loguser->access_token =$decoded_data['access_token'];
-        $loguser->refresh_token =$decoded_data['refresh_token'];
-        $loguser->scope =$decoded_data['scope'];
-        $loguser->line_user_id = "";
-        $loguser->expires_in =$decoded_data['expires_in'];
-       
+         $logUser =new LoginUser;
+         $loguser->access_token =$decoded_data['access_token'];
+         $loguser->refresh_token =$decoded_data['refresh_token'];
+         $loguser->scope =$decoded_data['scope'];
+         $loguser->line_user_id = "";
+         $loguser->expires_in =$decoded_data['expires_in'];
 
-        $logUser ->save();
-         */
 
-        $dData=$this->getProf($access_token);
+         $logUser ->save();
+          */
+
+        $up=$this->getProf($access_token);
         
-        $ups=new UserProf;
-        $ups->line_user_id=$dData['userId'];
-        $ups->line_user_name=$dData['displayName'];
-        $ups->prof_img_url=$dData['pictureUrl'];
-        $ups->prof_msg=$dData['statusMessage'];
         
-        return view('getUser',[
-            'users' =>$ups,
+        return view('getUser', [
+            'users' =>$up,
         ]);
 
-      //  return view('getUser');
+        //  return view('getUser');
     }
 
-/**
- *  function getProfile:アクセストークンからユーザのプロフィールを取得
- *
- * @param [type] $access_token
- * @return view
- */
-    public function getProf($access_token){
-
-
-
+    /**
+     *  function getProfile:アクセストークンからユーザのプロフィールを取得
+     *
+     * @param [type] $access_token
+     * @return view
+     */
+    public function getProf($access_token)
+    {
         $api_url ='https://api.line.me/v2/profile';
 
         //GETでリクエストする。
@@ -124,7 +118,7 @@ class Login extends Controller
         curl_setopt($curl_handle, CURLOPT_HTTPGET, true);
         curl_setopt($curl_handle, CURLOPT_URL, $api_url);
         curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $headers);
-                // curl_exec()の結果を文字列にする
+        // curl_exec()の結果を文字列にする
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, true);
         
 
@@ -136,43 +130,37 @@ class Login extends Controller
 
         //デコード
         $decoded_data = json_decode($json_response, true);
-        $out=json_decode($json_response, true);
        
         //取得したプロフィールを保存
         //TODO:値が取れなかった場合の処理も実装する
-       $up = new UserProf;
-       $up->line_user_id=$decoded_data['userId'];
-       $up->line_user_name=$decoded_data['displayName'];
-       $up->prof_img_url=$decoded_data['pictureUrl'];
-       $up->prof_msg=$decoded_data['statusMessage'];
+        $up = new UserProf;
+        $up->line_user_id=$decoded_data['userId'];
+        $up->line_user_name=$decoded_data['displayName'];
+        $up->prof_img_url=$decoded_data['pictureUrl'];
+        $up->prof_msg=$decoded_data['statusMessage'];
     
-       //取得不可なので空文字でinsert
-       $up->user_os="";
-     $up->user_trans="";
-       $up->save();
+        //取得不可なので空文字でinsert
+        $up->user_os="";
+        $up->user_trans="";
+        $up->save();
 
         //Login_userテーブルにuserIdを格納(pivotつくるわ)
-      /*
-        $this->logU=DB::table('login_users')
-       ->select('*')
-       ->where('access_token',$access_token)
-       ->orderBy('created_at','DESC')
-       ->limit(1)
-       ->get();
-
-       
-        $this->logU=LoginUser::where('access_token',$access_token)
-        ->orderBy('created_at','DESC')
-        ->first();
-  
-       $this->logU->line_user_id->fill($decoded_data['userId'])->save();
-         */
+        /*
+          $this->logU=DB::table('login_users')
+         ->select('*')
+         ->where('access_token',$access_token)
+         ->orderBy('created_at','DESC')
+         ->limit(1)
+         ->get();
 
 
-return $out;
+          $this->logU=LoginUser::where('access_token',$access_token)
+          ->orderBy('created_at','DESC')
+          ->first();
 
+         $this->logU->line_user_id->fill($decoded_data['userId'])->save();
+           */
+
+        return $up;
     }
-
-
-
 }
