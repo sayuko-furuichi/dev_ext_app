8b68137ff8312e0ce3cc0fb6191a34efc40d3a6f
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 17:16:21 +0900

    ああ

--- resources/views/flowdemo/official.blade.php
+++ resources/views/flowdemo/official.blade.php
@@ -16,19 +16,23 @@
     <h2>公式アカウント始めました</h2>
     <h3>以下のバナー、リンク、QRコードから遷移できます</h3>
 
-    
-    <br>
-    <img src="https://qr-official.line.me/sid/M/060rxmbw.png?shortenUrl=true&appendQueryParameter=oat__id%3D1053725">
-    <br>
-    <a href="https://lin.ee/rWSlcAN">公式アカウントを友達登録する</a>
-    <br>
-    <a href="https://lin.ee/CFPxGIJ"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
-    <br>
-    
-    <p>Have a nice day!</p>
 
-    <p style="color: darkblue">PC版ではQRコードへ遷移、スマートフォン版では、友達登録画面へ遷移します</p>
+    <p></p>
+    <img src="https://qr-official.line.me/sid/M/060rxmbw.png?shortenUrl=true&appendQueryParameter=oat__id%3D1053725">
+  <p></p>
+    <a href="https://lin.ee/rWSlcAN">公式アカウントを友達登録する</a>
+    <p></p>
+    <a href="https://lin.ee/CFPxGIJ"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
+    <p></p>
+    <p></p>
+    <p></p>
+    <p>Have a nice day!</p>
+    <p></p>
+    <p style="color: darkblue">リンク先：PC版ではQRコードへ遷移、スマートフォン版では、友達登録画面へ遷移します</p>
     {{-- いらんかな？ --}}
+    
+    <p></p>
+    <p></p>
     <a href="{{ route('flows.index') }}">LPがあるページへ戻る</a>
 
 </body>
