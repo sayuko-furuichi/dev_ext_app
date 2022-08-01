cf3894638ecd245492eae4cef560bf4a2359ae94
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 15:31:28 +0900

    controller　作成

--- app/Http/Controllers/Flows.php
+++ app/Http/Controllers/Flows.php
@@ -7,4 +7,14 @@ use Illuminate\Http\Request;
 class Flows extends Controller
 {
     //
+
+    public function index(){
+
+        return view ('flowdemo.lppage');
+    }
+
+    public function off(){
+
+        return view ('flow.off');
+    }
 }
