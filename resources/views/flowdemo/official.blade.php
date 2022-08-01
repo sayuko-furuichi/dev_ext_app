401e094e8015215539d613d861654cfc26e09c4c
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 15:44:17 +0900

    textarea

--- resources/views/flowdemo/official.blade.php
+++ resources/views/flowdemo/official.blade.php
@@ -12,6 +12,7 @@
 <h1>OFFICIAL SITE</h1>
 
 <a href="https://lin.ee/rWSlcAN">公式アカウントを友達登録する</a>
+<textarea cols="30" rows="10">PC版ではQRコードへ遷移、スマートフォン版では、友達登録画面へ遷移します</textarea>
 
 
 <p>Have a nice day!</p>
