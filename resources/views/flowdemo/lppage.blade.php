18f06f1bca86660098ba25ae7ba79c189c07b67f
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 16:26:47 +0900

    ss

--- resources/views/flowdemo/lppage.blade.php
+++ resources/views/flowdemo/lppage.blade.php
@@ -11,9 +11,10 @@
 <body align="left" margin="10rem"> 
 <h1>WELCOME</h1>
 <div align="center">
-<img src="https://dev-ext-app.herokuapp.com/storage/app/public/greenlp.png" alt="picture" width="30%" href="{{route('flows.off')}}">
+    <a href="{{route('flows.off')}}"><img src="https://dev-ext-app.herokuapp.com/storage/app/public/greenlp.png" alt="picture" width="30%"></a>
+
 </div>
-    <a href="{{route('flows.off')}}"></a>
+   
     <p>Hello!!! world!!!! </p>
 </body>
 
