b526c1f258bf6558c2a7a19155c7503600ba5723
sayuko-furuichi <zack.dog009@gmail.com>
2022-08-01 15:56:20 +0900

    ok

--- resources/views/flowdemo/official.blade.php
+++ resources/views/flowdemo/official.blade.php
@@ -8,24 +8,28 @@
     {{-- <script src="https://unpkg.com/glottologist"></script> --}}
     <title>cafeDEMO OFFICIAL SITE</title>
 </head>
+
 <body align="center" width="80%">
-<h1 style="font-style: fantasy">OFFICIAL SITE</h1>
+    <h1 style="font-family: fantasy">OFFICIAL SITE</h1>
 
-<hr>
-<h2>公式アカウント始めました</h2>
-<h3>以下のバナー、リンクから遷移できます</h3>
+    <hr>
+    <h2>公式アカウント始めました</h2>
+    <h3>以下のバナー、リンクから遷移できます</h3>
 
-<a href="https://lin.ee/rWSlcAN">公式アカウントを友達登録する</a>
-<p style="color: darkblue">PC版ではQRコードへ遷移、スマートフォン版では、友達登録画面へ遷移します</p>
+    <a href="https://lin.ee/rWSlcAN">公式アカウントを友達登録する</a>
+    <a href="https://lin.ee/rWSlcAN"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加"
+            height="36" border="0"></a>
 
-<a href="https://lin.ee/rWSlcAN"><img src="https://scdn.line-apps.com/n/line_add_friends/btn/ja.png" alt="友だち追加" height="36" border="0"></a>
+    <br>
+    <br>
 
-<p>Have a nice day!</p>
 
+    <p>Have a nice day!</p>
 
-{{--  いらんかな？  --}}
-<a href="{{route('flows.index')}}">LPがあるページへ戻る</a>
-    
+    <p style="color: darkblue">PC版ではQRコードへ遷移、スマートフォン版では、友達登録画面へ遷移します</p>
+    {{-- いらんかな？ --}}
+    <a href="{{ route('flows.index') }}">LPがあるページへ戻る</a>
+
 </body>
 
 </html>
