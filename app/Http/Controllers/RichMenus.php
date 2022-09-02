3754f9e34e9838130b23c912fe219b54ad863d9d
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-02 12:19:02 +0900

    damedesu

--- app/Http/Controllers/RichMenus.php
+++ app/Http/Controllers/RichMenus.php
@@ -67,6 +67,8 @@ class RichMenus extends Controller
 
         $request=json_decode($request,true);
         $sss=json_decode($_POST,true);
+        $var_dump($sss);
+        $var_dump($request);
 
         return view('sendEvents.richMenuMng',[
             'rmList'=>$sss
