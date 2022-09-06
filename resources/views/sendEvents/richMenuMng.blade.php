7d9da7544f96646755c9c243cf98f8dc084a40f2
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-06 17:16:01 +0900

    ok

--- resources/views/sendEvents/richMenuMng.blade.php
+++ resources/views/sendEvents/richMenuMng.blade.php
@@ -31,6 +31,7 @@
     <div>
         <table border="3">
             <tr>
+            <th> </th>
             <th>name</th>
             <th>chat_bar</th>
             <th>is_default</th>
@@ -41,6 +42,7 @@
           @foreach ($rmList as $rm)
           {{--  エイリアスIDでまとめる方法：aだけ取得する→aと一致するもの取得  --}}
         <tr>
+            <td><input type="radio" name="rich" value={{$rm->rich_menu_id}}></td>
             <td><img src="{{secure_asset('img/'.$rm->img)}}" alt="img" width="30%"> </td>
         @if ($rm->is_default==1)
         <td><font color="red">{{$rm->name}}</td></font>
