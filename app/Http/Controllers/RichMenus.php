c99fb85a798362fb214148d0c41d072e7f529411
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-06 15:04:59 +0900

    とりまindexはreturnのみ

--- app/Http/Controllers/RichMenus.php
+++ app/Http/Controllers/RichMenus.php
@@ -9,65 +9,6 @@ class RichMenus extends Controller
     //
     public function index(){
 
-        $detail=([
-
-            'destination'=> 'Uffd4dd52c580e1d2bb7b0a66e0ef1951',
-            'events'=> [
-              [
-                'type'=> 'message',
-                'message'=> [
-                  'type'=> 'text',
-                  'text'=>  'plz RichMenus',
-             //     'text2'=>  $request->msg2
-              ],
-                'timestamp'=> $_SERVER['REQUEST_TIME'],
-                'source'=> [
-                  'type'=> 'web',
-                ],
-  //              'replyToken'=> $rand_str,
-                'mode'=> 'active',
-           //     'webhookEventId'=> $rand_str,
-                'deliveryContext'=> [
-                  'isRedelivery'=> false
-                ]
-              ],
-    
-            ]
-          ]);
-    
-         $header = array(
-             'Content-Type: application/json',
-           'x_demo_signature: demo',
-         );
-    
-         //試しに、create richmenuにする
-         $context = stream_context_create([
-             'http' => [
-                 'ignore_errors' => true,
-                 'method' => 'POST',
-                 'header' => implode("\r\n", $header),
-                 'content' => json_encode($detail, true)
-             ],
-         ]);
-        
-    
-     $res=  file_get_contents('https://dev-bot0722.herokuapp.com/public/api/callback?store_id=3', false, $context);
-
-     $res=json_decode($res,true);
-      //  $sss=json_decode($_POST,true);
-        // dd($_POST);
-        // $var_dump($res);
-
-        return view('sendEvents.richMenuMng',[
-            'rmList'=>$res
-        ]);
-    
-         if (strpos($http_response_header[0], '200') === false) {
-            //  $rmList = 'false';
-         }
-       //  $rmList=json_decode($rmList,true);
-        
-
         return view('sendEvents.richMenuMng'
         );
     }
@@ -82,5 +23,64 @@ class RichMenus extends Controller
         return view('sendEvents.richMenuMng',[
             'rmList'=>$sss
         ]);
+    }
+
+    public function send(){
+      $detail=([
+
+        'destination'=> 'Uffd4dd52c580e1d2bb7b0a66e0ef1951',
+        'events'=> [
+          [
+            'type'=> 'message',
+            'message'=> [
+              'type'=> 'text',
+              'text'=>  'plz RichMenus',
+         //     'text2'=>  $request->msg2
+          ],
+            'timestamp'=> $_SERVER['REQUEST_TIME'],
+            'source'=> [
+              'type'=> 'web',
+            ],
+//              'replyToken'=> $rand_str,
+            'mode'=> 'active',
+       //     'webhookEventId'=> $rand_str,
+            'deliveryContext'=> [
+              'isRedelivery'=> false
+            ]
+          ],
+
+        ]
+      ]);
+
+     $header = array(
+         'Content-Type: application/json',
+       'x_demo_signature: demo',
+     );
+
+     //試しに、create richmenuにする
+     $context = stream_context_create([
+         'http' => [
+             'ignore_errors' => true,
+             'method' => 'POST',
+             'header' => implode("\r\n", $header),
+             'content' => json_encode($detail, true)
+         ],
+     ]);
+    
+
+ $res=  file_get_contents('https://dev-bot0722.herokuapp.com/public/api/callback?store_id=3', false, $context);
+
+ $res=json_decode($res,true);
+  //  $sss=json_decode($_POST,true);
+    // dd($_POST);
+    // $var_dump($res);
+
+   
+
+     if (strpos($http_response_header[0], '200') === false) {
+        //  $rmList = 'false';
+     }
+   //  $rmList=json_decode($rmList,true);
+
     }
 }
