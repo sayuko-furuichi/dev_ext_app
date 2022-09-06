b42de28cdd65982d04af9d59c9f3d622e42d4033
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-06 18:40:40 +0900

    a

--- resources/views/sendEvents/richMenuMng.blade.php
+++ resources/views/sendEvents/richMenuMng.blade.php
@@ -8,7 +8,6 @@
      {{--  <link rel="stylesheet" href="{{ secure_asset('css/liff.css') }}">    --}}
     {{--  <link rel="stylesheet" href="{{ secure_asset('css/reset.css') }}">    --}}
     {{--  <link id="import-link" rel="import" href="./sub.html">  --}}
-    <script src="https://unpkg.com/glottologist"></script> 
     <title>RMM</title>
 </head>
 
@@ -61,12 +60,7 @@
       
     </div>
  
-    
 
-    {{--  <script charset="utf-8" src="https://static.line-scdn.net/liff/edge/2/sdk.js"></script>  --}}
-    <script src="js/jquery-1.9.0.min.js" type="text/javascript"></script>
-    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-    {{--  <script src="js/home.js"></script>  --}}
 </body>
 
 </html>
