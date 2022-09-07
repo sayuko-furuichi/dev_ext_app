0d2a375c4ef7959cfa62825c025044ac15fadce3
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-07 13:11:45 +0900

    flashメッセージ設定

--- app/Http/Controllers/RichMenus.php
+++ app/Http/Controllers/RichMenus.php
@@ -44,33 +44,11 @@ class RichMenus extends Controller
 
     public function send(Request $request){
       $this->storeId=3;
-//       $detail=([
 
-//         'destination'=> 'Uffd4dd52c580e1d2bb7b0a66e0ef1951',
-//         'events'=> [
-//           [
-//             'type'=> 'message',
-//             'message'=> [
-//               'type'=> 'text',
-//               'text'=>  'change_df_rich_menu',
-//               'text2'=>  $request->id
-//           ],
-//             'timestamp'=> $_SERVER['REQUEST_TIME'],
-//             'source'=> [
-//               'type'=> 'web',
-//             ],
-// //              'replyToken'=> $rand_str,
-//             'mode'=> 'active',
-//        //     'webhookEventId'=> $rand_str,
-//             'deliveryContext'=> [
-//               'isRedelivery'=> false
-//             ]
-//           ],
+//デフォルト設定反映
 
-//         ]
-//       ]);
 
-//TODO:後で消す
+//context作成
 
 $detail=([
 
@@ -118,16 +96,15 @@ $detail=([
 
  $res=  file_get_contents('https://dev-bot0722.herokuapp.com/public/api/callback?store_id='.$this->storeId, false, $context);
 
- var_dump($res);
-
- $res=json_decode($res,true);
   //  $sss=json_decode($_POST,true);
     // dd($_POST);
     // $var_dump($res);
      if (strpos($http_response_header[0], '200') === false) {
-        //  $rmList = 'false';
+        $msg='失敗しました'
+     }else{
+      $msg='成功しました！'
      }
-   return redirect('/rich');
+   return redirect('/rich')->with('flash_message',$msg);
 
     }
 }
