b74855094d4482a4fd5a5e46c9ff8e7d19bbb975
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-12 17:22:59 +0900

    a

--- app/Http/Controllers/Cats.php
+++ app/Http/Controllers/Cats.php
@@ -48,7 +48,7 @@ class Cats extends Controller
         //CATからLIFFアプリを追加する
 
 
-        $cat = new Cat;
+        $cat = new Cat();
         $cat->cat=$resj['access_token'];
         $cat->channel_id= '1657463796';
         $cat->liff_cs='4cb5a01c2509810b67a8b98e6a88efa3';
@@ -57,35 +57,48 @@ class Cats extends Controller
         return view('serverApi.addLiff', [
             'token'=>$resj['access_token']
         ]);
-        $res=$this->addLiff();
+        $res=$this->addLiff($cat);
 
         //  return redirect('/serve')->with('token',$res->access_token);
     }
 
-    public function addLiff()
+    public function addLiff($cat)
     {
-        $param =[
-            'grant_type'=>'client_credentials',
-            'client_id' => '1657463796',
-            'client_secret' => '4cb5a01c2509810b67a8b98e6a88efa3'
-
+        $param=[
+            'view'=> [
+                'type'=> 'full',
+                'url'=> 'https://example.com/myservice'
+            ],
+            'description'=> 'APIで作ったLIFF',
+            'features'=> [
+                'ble'=> false,
+                'qrCode'=> true
+            ],
+            'permanentLinkPattern'=> 'concat',
+            'scope'=> ['profile', 'chat_message.write'],
+            'botPrompt'=> 'normal'
         ];
+
+
         $header = array(
-            'Content-Type: application/x-www-form-urlencoded',
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $cat->cat,
         );
         $context = stream_context_create([
             'http' => [
                 'ignore_errors' => true,
                 'method' => 'POST',
                 'header' => $header,
-               'content' => json_encode($param)
+                // JSON_UNESCAPED_UNICODE？
+               'content' => json_encode($param),
             ],
         ]);
 
-        $res=file_get_contents('https://api.line.me/liff/v1/apps', false, $context);
+        $res=file_get_contents('https://api.line.me/v2/bot/audienceGroup/upload', false, $context);
         if (strpos($http_response_header[0], '200') === false) {
-            $res='request failed';
-            return $res;
+              //     $res='request failed';
         }
+
+        return $res;
     }
 }
