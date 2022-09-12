7b8fa7ca9299c4e237e078f85b26b7d4f4a268f8
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-12 15:23:49 +0900

    post先cont

--- app/Http/Controllers/Cats.php
+++ app/Http/Controllers/Cats.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 class Cats extends Controller
 {
     //
-    public function index(){
-    return view('serverApi.addLiff');
+    public function index()
+    {
+        return view('serverApi.addLiff');
+    }
 
+    public function getCat(Request $request)
+    {
+        return redirect('/serve');
 
     }
 }
