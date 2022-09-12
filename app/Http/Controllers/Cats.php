800314299264fdfda32b89967381add40c741fad
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-12 15:29:20 +0900

    add:use

--- app/Http/Controllers/Cats.php
+++ app/Http/Controllers/Cats.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cat;
 
 class Cats extends Controller
 {
@@ -14,6 +15,9 @@ class Cats extends Controller
 
     public function getCat(Request $request)
     {
+
+
+
         return redirect('/serve');
 
     }
