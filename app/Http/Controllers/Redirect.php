dd232b35c9dcf0e26b30ddd542a93213203c416a
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-26 16:18:11 +0900

    zzz

--- app/Http/Controllers/Redirect.php
+++ app/Http/Controllers/Redirect.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Store;
+use App\Models\Route;
+use App\Models\LineuserInflowRoute;
 
 class Redirect extends Controller
 {
     //
     function redirect(Request $request){
+       $store= Store::where('id',$request->store)->first();
+       $route=Route::where('id',$request->route)->first();
 
-        return redirect('https://lin.ee/8zMNLcl');
+       $inflow = new LineuserInflowRoute;
+       $inflow->store=$store->id;
+       $inflow->route=$route->route_name;
+       $inflow->save();
+
+        return redirect($store->account_url);
     }
 }
