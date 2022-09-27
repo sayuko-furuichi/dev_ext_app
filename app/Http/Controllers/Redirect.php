9bab127016abc873684421215c7525a993571d92
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-27 13:33:44 +0900

    aa

--- app/Http/Controllers/Redirect.php
+++ app/Http/Controllers/Redirect.php
@@ -8,7 +8,8 @@ use App\Models\Route;
 use App\Models\LineuserInflowRoute;
 
 // use App\SimpleSoftwareIO\QrCode\QrCodeServiceProvider;
- use SimpleSoftwareIO\QrCode\Facades\QrCode;
+// use SimpleSoftwareIO\QrCode\Facades\QrCode;
+ use App\vendor\simplesoftwareio\src\Facades\QrCode;
 
 
 class Redirect extends Controller
