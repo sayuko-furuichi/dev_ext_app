41e7ba04e032a742401ea63d9f34e247202dddb5
sayuko-furuichi <zack.dog009@gmail.com>
2022-09-27 16:32:23 +0900

    a

--- app/Http/Controllers/Redirect.php
+++ app/Http/Controllers/Redirect.php
@@ -22,7 +22,7 @@ class Redirect extends Controller
        $inflow->route=$route->route_name;
        $inflow->save();
 
-        return url($store->account_url);
+        return redirect($store->account_url);
     }
 
     function index(){
